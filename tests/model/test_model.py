3cee6bc53f350ff6d93b6f882a029d14340cf680
Anna Kvashchuk <kvashchuk.anna@gmail.com>
2025-04-28 17:12:52 +0200

    Add log interpolation and compare with true values

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -78,8 +78,8 @@ def test_pressure_and_temperature_values_are_between():
 
 def test_interpolation_gibbs():
     results = []
-    pressure_list = [30, 35]
-    temperature_list = [200, 250]
+    pressure_list = [30, 40]
+    temperature_list = [200, 300]
     tp_combinations = []
 
     for p in pressure_list:
@@ -103,12 +103,50 @@ def test_interpolation_gibbs():
     interpolators = [LinearNDInterpolator(tp_combinations, np.array(df_grouped.iloc[i, 1:5])) for i in range(len(df_grouped))]
 
     # Specify a point (x, y) where you want to get the interpolated values
-    target_pressure = 31
-    target_temperature = 225
+    target_pressure = 35
+    target_temperature = 250
 
     # Get the interpolated values for the specified point for each set of values
-    interpolated_values = [interpolator(target_temperature, target_pressure) for interpolator in interpolators]
+    linearND_values = [interpolator(target_temperature, target_pressure) for interpolator in interpolators]
+
+    low_pressure_gibbs = [np.interp(target_temperature, temperature_list, [float(df_grouped.iloc[i, 1]), float(df_grouped.iloc[i, 2])]) for i in range(len(df_grouped))]
+    high_pressure_gibbs = [np.interp(target_temperature, temperature_list, [float(df_grouped.iloc[i, 3]), float(df_grouped.iloc[i, 4])]) for i in range(len(df_grouped))]
+
+    final_gibbs_linear_interp = [np.interp(target_pressure, pressure_list, [float(low_pressure_gibbs[i]), float(high_pressure_gibbs[i])]) for i in range(len(df_grouped))]
+
+    final_gibbs_log = [log_pressure_interpolation(target_pressure, pressure_list, [float(low_pressure_gibbs[i]), float(high_pressure_gibbs[i])]) for i in range(len(df_grouped))]
+
+    if (np.isclose(linearND_values[i], final_gibbs_linear_interp[i]) for i in range(len(linearND_values))):
+        pass
+    else:
+        print(f"linearND_values = {linearND_values}, final_gibbs = {final_gibbs_linear_interp}")
+
+    if (np.isclose(final_gibbs_linear_interp[i], final_gibbs_log[i]) for i in range(len(linearND_values))):
+        pass
+    else:
+        print(f"final_gibbs_linear_interp = {final_gibbs_linear_interp}, final_gibbs_log = {final_gibbs_log}")
 
     # Print the results
-    for j, value in enumerate(interpolated_values):
-        print(f"Interpolated value at point ({target_temperature}, {target_pressure}) for Set {j + 1}: {value:.2f}")
+    #for j, value in enumerate(linearND_values):
+    #    print(f"Linearly interpolated value at point ({target_temperature}, {target_pressure}) for Set {j + 1}: {value:.2f}")
+
+    true_reactions = get_reactions(target_temperature, target_pressure)
+    for reaction_id in range(len(true_reactions)):
+        g_value = true_reactions[reaction_id]['g']
+        assert np.isclose(g_value, linearND_values[reaction_id], atol=0.1, rtol=0.01)
+        assert np.isclose(g_value, final_gibbs_linear_interp[reaction_id], atol=0.1, rtol=0.01)
+        assert np.isclose(g_value, final_gibbs_log[reaction_id], atol=0.1, rtol=0.01)
+
+
+
+def log_pressure_interpolation(target: float, x: list, y: list):
+    '''
+        Interpolating y = a + b log x
+        Assuming x = [x0, x1] and y = [y0, y1]
+    '''
+    b = (y[1] - y[0])/(np.log(x[1]) - np.log(x[0]))
+    a = y[0] - b * np.log(x[0])
+    log_interpolated_value = a + b * np.log(target)
+    return log_interpolated_value
+
+
