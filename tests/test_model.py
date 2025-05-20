31af651e4c5153a636a4aee58d8688a372e50176
Lars Petter Hauge <larsenhauge@gmail.com>
2025-05-20 10:50:00 +0200

    Suggested changes

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+import copy
 
 from arcs.model import (
     _find_enclosing,
@@ -38,7 +39,7 @@ TEMPERATURE_LIST = [200, 250, 300, 350, 400]
     "target, expected_lower, expected_upper",
     [
         (223.6, 200, 250),
-        (200.0, 200, 250),
+        (200.0, 200, 200),
         (275.0, 250, 300),
         (399.9, 350, 400),
     ],
@@ -49,22 +50,43 @@ def test_find_enclosing(target: float, expected_lower: int, expected_upper: int)
     assert upper == expected_upper
 
 
+def test_interpolated_handle_points_in_list():
+    # We only assert the structure of the return type
+    result = get_interpolated_reactions(TEMPERATURE_LIST[1], PRESSURE_LIST[1])
+
+    assert len(result) > 9000
+    assert isinstance(result, dict)
+    assert all(x in result[0] for x in ["e", "g", "k"])
+
+
+def test_interpolated_handle_single_axis():
+    _ = get_interpolated_reactions(TEMPERATURE_LIST[1] + 1, PRESSURE_LIST[1])
+
+
 def test_interpolated_result_accuracy():
-    for temp in range(len(TEMPERATURE_LIST) - 1):
-        for press in range(len(PRESSURE_LIST) - 1):
-            temperature = TEMPERATURE_LIST[temp]
-            pressure = PRESSURE_LIST[press]
+    for temperature in TEMPERATURE_LIST[1:-1]:
+        for pressure in PRESSURE_LIST[1:-1]:
+            mocked_temperatures = copy.copy(TEMPERATURE_LIST)
+            mocked_pressures = copy.copy(PRESSURE_LIST)
+            mocked_temperatures.remove(temperature)
+            mocked_pressures.remove(pressure)
 
-            true_reactions = get_reactions(temperature, pressure)
-            interpolated_reactions = get_interpolated_reactions(temperature, pressure)
+            with pytest.MonkeyPatch.context() as m:
+                m.setattr("arcs.model.PRESSURE_LIST", mocked_pressures)
+                m.setattr("arcs.model.TEMPERATURE_LIST", mocked_temperatures)
 
-            assert [
-                np.isclose(
-                    interpolated_reactions[reaction_id]["g"],
-                    true_reactions[reaction_id]["g"],
+                true_reactions = get_reactions(temperature, pressure)
+                interpolated_reactions = get_interpolated_reactions(
+                    temperature, pressure
                 )
-                for reaction_id in range(len(true_reactions))
-            ]
+
+                assert [
+                    np.isclose(
+                        interpolated_reactions[reaction_id]["g"],
+                        true_reactions[reaction_id]["g"],
+                    )
+                    for reaction_id in range(len(true_reactions))
+                ]
 
 
 def test_calculate_k():
