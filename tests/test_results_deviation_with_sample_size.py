8e199d53229bb2e21cfbcd919e4bb9b3d9f0c632
Anna Kvashchuk <kvashchuk.anna@gmail.com>
2025-08-28 10:28:55 +0200

    Improve testing of sample size

--- tests/test_results_deviation_with_sample_size.py
+++ tests/test_results_deviation_with_sample_size.py
@@ -1,12 +1,15 @@
 import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
 
 from arcs.traversal import traverse
 from arcs.analysis import AnalyseSampling
 
+sample_sizes = [10, 100, 500]
+
 def test_sample_size_deviation():
     #sample_sizes = [500, 1000, 1500, 2000]
     #n = 100
-    sample_sizes = [10, 15, 20]
 
     pressure = 10
     temperature = 300
@@ -19,7 +22,8 @@ def test_sample_size_deviation():
         "H2S": 5.0e-6,
     }
 
-    result_stats = {}
+    mean_values = {}
+    std_values = {}
 
     for s in sample_sizes:
         results = traverse(
@@ -34,24 +38,81 @@ def test_sample_size_deviation():
         mean = analysis.final_concs_mean
         print(s, std, mean)
 
+        for component in mean.keys():
+            if component not in mean_values:
+                mean_values[component] = []
+                std_values[component] = []
 
-    print(result_stats)
-    test_plot_mean_std(result_stats, ["H2O", "H2SO4"])
+            mean_values[component].append(mean[component])
+            std_values[component].append(std[component])
+
+    component_list = ["H2O", "H2SO4"]
+    plot_mean_std_from_component_list(component_list, mean_values, std_values)
 
 
-def run_arcs(pressure, temperature, concentrations, sample_size):
-    results = traverse(
-        temperature,
-        pressure,
-        concentrations,
-        samples=sample_size,
-    )
-    std_mean_per_chem = {}
-    std_dict = calculate_standard_deviation(results.data)
-    for chem in std_dict.keys():
-        std_mean_per_chem[chem] = [results.final_concs[chem], std_dict[chem]]
+def plot_mean_std_from_component_list(component_list, mean_values, std_values):
+    # Plotting
+    plt.figure(figsize=(12, 6))
 
-    return std_mean_per_chem
+    # Check sizes before plotting
+    for component in component_list:
+        print(f"Component: {component}")
+        print(f"Sample Sizes: {sample_sizes}")
+        print(f"Mean Values: {mean_values[component]}")
+        print(f"Std Values: {std_values[component]}")
+
+        # Check if lengths match
+        if len(sample_sizes) == len(mean_values[component]) and len(sample_sizes) == len(std_values[component]):
+            plt.errorbar(sample_sizes, mean_values[component], yerr=std_values[component],
+                         label=component, capsize=5, fmt='o', markersize=5)
+        else:
+            print(f"Warning: Sizes do not match for component {component}. Skipping this component.")
+
+    # Adding labels and title
+    plt.xlabel('Sample Size (s)')
+    plt.ylabel('Concentration')
+    plt.title('Mean and Standard Deviation of Concentrations vs Sample Size')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
+def test_std_of_final_concentrations_when_running_arcs_several_times(repeats=100):
+    pressure = 10
+    temperature = 300
+    concentrations = {
+        "CO2": 1.0,
+        "H2O": 40.0e-6,
+        "O2": 10.0e-6,
+        "SO2": 10.0e-6,
+        "NO2": 5.0e-6,
+        "H2S": 5.0e-6,
+    }
+    mean_values = {}
+    std_values = {}
+
+    for s in sample_sizes:
+        final_conc_n_runs = {}
+        for n in range(repeats):
+            results = traverse(
+                temperature,
+                pressure,
+                concentrations,
+                samples=s,
+            )
+            for chemical in results.final_concs.keys():
+                if chemical not in final_conc_n_runs:
+                    final_conc_n_runs[chemical] = []
+                final_conc_n_runs[chemical].append(results.final_concs[chemical])
+
+        for chemical in final_conc_n_runs.keys():
+            if chemical not in mean_values:
+                mean_values[chemical] = []
+                std_values[chemical] = []
+            mean_values[chemical].append(np.mean(final_conc_n_runs[chemical]))
+            std_values[chemical].append(np.std(final_conc_n_runs[chemical]))
+
+    plot_mean_std_from_component_list(["H2O", "H2SO4"], mean_values, std_values)
 
 
 def calculate_standard_deviation(data_dict):
@@ -70,12 +131,7 @@ def calculate_standard_deviation(data_dict):
     return conc_std
 
 
-import matplotlib.pyplot as plt
-
 def test_plot_mean_std(result_stats, compounds):
-    sample_sizes = list(result_stats.keys())
-    means = []
-    stds = []
     # Create the plot
     plt.figure(figsize=(8, 6))
     # Collect mean and std for the specified compound
@@ -101,6 +157,68 @@ def test_plot_mean_std(result_stats, compounds):
     plt.xscale('linear')
     #plt.yscale('log')  # Use logarithmic scale if values vary greatly
     plt.show()
+
+
+def test_deviations_inside_arcs_with_multiple_runs(n=10):  # Added n as a parameter with a default value
+    pressure = 10
+    temperature = 300
+    concentrations = {
+        "CO2": 1.0,
+        "H2O": 40.0e-6,
+        "O2": 10.0e-6,
+        "SO2": 10.0e-6,
+        "NO2": 5.0e-6,
+        "H2S": 5.0e-6,
+    }
+
+    mean_values = {}
+    std_values = {}
+
+    for s in sample_sizes:
+        all_means = []
+        all_stds = []
+
+        for _ in range(n):  # Run traverse n times for each sample size
+            results = traverse(
+                temperature,
+                pressure,
+                concentrations,
+                samples=s,
+            )
+            analysis = AnalyseSampling(results.data)
+            analysis.output_concentrations_std_mean()
+            std = analysis.final_concs_std
+            mean = analysis.final_concs_mean
+
+            all_means.append(mean)
+            all_stds.append(std)
+
+        #######
+        # Collect all unique components from means and stds
+        all_components = set()
+        for mean in all_means:
+            all_components.update(mean.keys())
+        for std in all_stds:
+            all_components.update(std.keys())
+
+        # Average the means and standard deviations over n runs for each unique component
+        avg_mean = {component: sum(d.get(component, 0) for d in all_means) / n for component in all_components}
+        avg_std = {component: sum(d.get(component, 0) for d in all_stds) / n for component in all_components}
+
+        #######
+
+        print(s, avg_std, avg_mean)
+
+        for component in avg_mean.keys():
+            if component not in mean_values:
+                mean_values[component] = []
+                std_values[component] = []
+
+            mean_values[component].append(avg_mean[component])
+            std_values[component].append(avg_std[component])
+
+    component_list = ["H2O", "H2SO4"]
+    plot_mean_std_from_component_list(component_list, mean_values, std_values)
 
 
 
